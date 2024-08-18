79a9a5a005bdbcc72021f97e0aad118e0ceb786b
Nas-virat <napas.vin@gmail.com>
2024-08-18 13:05:45 +0700

    add tmux and wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,68 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- my coolnight colorscheme
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.font_size = 14
+
+config.window_background_opacity = 0.9
+
+local act = wezterm.action
+
+config.keys = {
+	-- Create a new tab in the same domain as the current pane.
+	-- This is usually what you want.
+	{
+		key = "t",
+		mods = "SHIFT|ALT",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	-- Create a new tab in the default domain
+	{ key = "t", mods = "SHIFT|ALT", action = act.SpawnTab("DefaultDomain") },
+	-- Create a tab in a named domain
+	{
+		key = "t",
+		mods = "SHIFT|ALT",
+		action = act.SpawnTab({
+			DomainName = "unix",
+		}),
+	},
+
+	-- paste from the clipboard
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+
+	-- paste from the primary selection
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
+}
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
+
+return config
